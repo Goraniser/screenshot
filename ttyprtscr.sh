58c5c54b58ef33d5da62f6a98759595100e03b81
Goraniser <goraniser@protonmail.ch>
2026-06-25 03:03:03 +0900

    Update ttyprtscr.sh
    그냥

--- ttyprtscr.sh
+++ ttyprtscr.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 TIMESTAMP=$(date +'%Y%m%d_%H%M%S')
 FILENAME="tty_${TIMESTAMP}.png"
 TMPFILENAME="tty_${TIMESTAMP}.ppm"
@@ -10,10 +10,20 @@ PRINTMSG() {
     ISIMPORTANT="$2"
 
     if [ -z "$ISIMPORTANT" ]; then
-	printf "${TITLE}: ${MSGBODY}\n" >&2
+	printf "%s: %s\n" "${TITLE}" "${MSGBODY}" >&2
     else
-	printf "\033[1;31m${TITLE}: ${MSGBODY}\033[0m\n" >&2
+	printf "\e[1;31m%s: %s\e[0m\n" "${TITLE}" "${MSGBODY}" >&2
     fi
+}
+
+HASDEPENDENCIES() {
+    for cmd in fbcat magick tailscale hostname awk grep; do
+	if ! command -v "$cmd" >/dev/null 2>&1; then
+	    PRINTMSG "?\`${cmd}\' program" 1
+	    PRINTMSG "Requires: fbcat imagemagick tailscale inetutils awk grep"
+	    exit 1
+	fi
+    done
 }
 
 ISTTY() {
@@ -34,14 +44,15 @@ ISTTY() {
 }
 
 TAILDROP_SEND() {
-    local target_file="$1"
+    target_file="$1"
 
     if ! tailscale status >/dev/null 2>&1; then
 	PRINTMSG "?Tailscale" 1
-	return 1
+	return 0
     fi
 
-    TARGETDEV=$(tailscale status | grep -v -i "$HOSTNAME" | head -n 1 | awk '{print $2}')
+    CURHOST=$(hostname)
+    TARGETDEV=$(tailscale status | grep -v -i "$CURHOST" | head -n 1 | awk '{print $2}')
 
     if [ -z "$TARGETDEV" ]; then
 	PRINTMSG "?Phone"
@@ -60,6 +71,7 @@ TAILDROP_SEND() {
 }
 
 ISTTY
+HASDEPENDENCIES
 
 mkdir -p "$SAVETO"
 FULLPATH="$SAVETO/$FILENAME"
@@ -79,7 +91,7 @@ if ! magick $TMPPATH $FULLPATH; then
 fi
 PRINTMSG "File at $FULLPATH"
 
-rm -f $TMPPATH
+rm -f "$TMPPATH"
 PRINTMSG "Cleaned $TMPPATH up"
 
 TAILDROP_SEND "$FULLPATH"
